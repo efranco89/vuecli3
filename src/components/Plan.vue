27a735864ca62fabd19a186e569541eeed905b28
Enrique Franco <luisfrancoz1@gmail.com>
2020-01-02 12:02:59 -0500

    Ref communication between parents and child components

---
+++ src/components/Plan.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <div class="plan">
+      <div class="descripcion">
+        <span class='titulo'>{{nombre}}</span>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+    name: 'Plan',
+    props: {
+      nombre: {
+        type:String,
+        default: 'Plan 1 - principiante',
+        required: true
+      },
+      precio: Number
+    }
+  }
+
+</script>
